eaf34e17b4050d79d4dce87414dc8dfa1086958f
Szymon Kowalski <szkmk131@gmail.com>
2024-07-03 19:58:11 +0200

    Trying to fix deployment

--- migrate.sh
+++ migrate.sh
@@ -2,6 +2,7 @@
 #python3 -m venv venv
 # activate the virtual environment
 #source venv/bin/activate
+python3 -m ensurepip --upgrade
 pip install -r requirements.txt
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
