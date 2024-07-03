bf1d2f51f59a22e69a9aeb58465462fd3dc4698c
Szymon Kowalski <szkmk131@gmail.com>
2024-07-03 18:09:25 +0200

    Change migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,5 +1,7 @@
 # migrate.sh
+python3 -m venv venv
+# activate the virtual environment
+source venv/bin/activate
 pip install -r requirements.txt
-python3.9 manage.py migrate
-python3.9 manage.py collectstatic --noinput
-python3.9 test.py
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
