2c0d591e91df461b39c7b4c0f85f6ecd49779ad9
Szymon Kowalski <szkmk131@gmail.com>
2024-07-03 19:53:54 +0200

    Remove requirements

--- migrate.sh
+++ migrate.sh
@@ -4,4 +4,4 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 python3 manage.py migrate
-python3 manage.py collectstatic --noinput
+#python3 manage.py collectstatic --noinput
