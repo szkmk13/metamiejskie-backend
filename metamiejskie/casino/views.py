54bf90ec6e58029044d17bdc223fd6252d0f405b
Szymon Kowalski <szkmk131@gmail.com>
2024-07-04 17:03:21 +0200

    Refactor casino

--- metamiejskie/casino/views.py
+++ metamiejskie/casino/views.py
@@ -73,7 +73,7 @@ class CasinoViewSet(ListModelMixin, GenericViewSet):
     def play_high_card(self, request, *args, **kwargs):
         serializer = HighCardPlaySerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        game_object, _ = HighCard.objects.get_or_create(user=request.user, name=f"{request.user.id}high card")
+        game_object, _ = HighCard.objects.get_or_create(user=request.user)
         bet_amount = serializer.validated_data["bet_amount"]
         shuffle(self.DECK)
         next_card = self.DECK[0]
